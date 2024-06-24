060936a39bc0dcb1d9b46301b99aaffffe5ecbba
Marcio Roberto Marini de Sousa <marcio@publica-ro.com.br>
2024-06-24 17:47:20 -0400

    dados em json

---
+++ resources/data/Publicacoes.js
@@ -0,0 +1,84 @@
+const publicacoes = [
+    {
+        ID: 1428,
+        NUMERO: "122",
+        DESCRICAO: "DECRETO 122/2015",
+        EMENTA: "REGULAMENTA O ACESSO ÀS INFORMAÇÕES PREVISTO NOS ARTIGOS 5º, XXXIII E 216, § 2º, DA CONSTITUIÇÃO FEDERAL E NA LEI FEDERAL Nº 12.527, DE 18 DE NOVEMBRO DE 2011.",
+        ANO: 2015,
+        DTHRPUBLICADO: "2022-05-31 16:57:38",
+        documentos: [
+            {
+                ID: 1428,
+                PUBLICACAO: 1428,
+                TIPO: "PDF",
+                QTACESSOS: 1156,
+            },
+        ],
+    },
+    {
+        ID: 1429,
+        NUMERO: "12.527",
+        DESCRICAO: "LEI Nº 12.527, DE 18 DE NOVEMBRO DE 2011.",
+        EMENTA: "Regula o acesso a informações previsto no inciso XXXIII do art. 5o, no inciso II do § 3o do art. 37 e no § 2o do art. 216 da Constituição Federal; altera a Lei no 8.112, de 11 de dezembro de 1990; revoga a Lei no 11.111, de 5 de maio de 2005, e dispositivos da Lei no 8.159, de 8 de janeiro de 1991; e dá outras providências.",
+        ANO: 2011,
+        DTHRPUBLICADO: "2022-05-31 17:01:24",
+        documentos: [
+            {
+                ID: 1429,
+                PUBLICACAO: 1429,
+                TIPO: "PDF",
+                QTACESSOS: 863,
+            },
+        ],
+    },
+    {
+        ID: 1525,
+        NUMERO: "001",
+        DESCRICAO: "Termo de Adesão ",
+        EMENTA: "TERMO DE ADESÃO DE COMPROMISSO AO PROGRAMA BRASIL TRANSPARENTE",
+        ANO: 2013,
+        DTHRPUBLICADO: "2022-06-06 16:29:07",
+        documentos: [
+            {
+                ID: 1525,
+                PUBLICACAO: 1525,
+                TIPO: "PDF",
+                QTACESSOS: 7034,
+            },
+        ],
+    },
+    {
+        ID: 1526,
+        NUMERO: "001",
+        DESCRICAO: "REQUERIMENTO DE INFORMAÇÃO - SIC",
+        EMENTA: "REQUERIMENTO DE INFORMAÇÃO - SIC",
+        ANO: 2015,
+        DTHRPUBLICADO: "2022-06-06 16:31:11",
+        documentos: [
+            {
+                ID: 1526,
+                PUBLICACAO: 1526,
+                TIPO: "PDF",
+                QTACESSOS: 883,
+            },
+        ],
+    },
+    {
+        ID: 3725,
+        NUMERO: "002/2017",
+        DESCRICAO: "Decreto Legislativo nº 002/2017",
+        EMENTA: "Dispõe sobre o acesso público a informações da Câmara Municipal de Corumbiara, em cumprimento à Lei Federal nº 12.527, de 18 de novembro de 2011.",
+        ANO: 2017,
+        DTHRPUBLICADO: "2017-07-31 14:29:00",
+        documentos: [
+            {
+                ID: 3728,
+                PUBLICACAO: 3725,
+                TIPO: "PDF",
+                QTACESSOS: 3022,
+            },
+        ],
+    },
+];
+
+export default publicacoes;
