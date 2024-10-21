8af4f424f75d37089f3c60da5bead318b6d172e3
Marcio Roberto Marini de Sousa <marcio@publica-ro.com.br>
2024-10-21 15:40:03 -0400

    Simplificação tela pedidos

---
+++ resources/js/Components/EnunciadoPedidos.jsx
@@ -0,0 +1,27 @@
+import {
+    Card,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+} from "@material-tailwind/react";
+
+const EnunciadoPedidos = ({ title, resumo, icon = null }) => {
+    return (
+        <div>
+            {icon ? icon : <></>}
+            <Typography
+                className="text-gray-800 dark:text-white mb-2"
+                variant="h5"
+                color="blue-gray"
+            >
+                {title}
+            </Typography>
+            <Typography className="text-gray-800 dark:text-white">
+                {resumo}
+            </Typography>
+        </div>
+    );
+};
+
+export default EnunciadoPedidos;
--- resources/js/Pages/Aplicacoes/Esic/Index.jsx
+++ resources/js/Pages/Aplicacoes/Esic/Index.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import BaseLayout from "../../../Layouts/BaseLayout";
 import { Head, usePage } from "@inertiajs/react";
-import {
-    Card,
-    CardBody,
-    CardHeader,
-    Typography,
-} from "@material-tailwind/react";
+import { Card, CardBody, CardHeader } from "@material-tailwind/react";
 import { SortableTable } from "../../../Components/SortableTable";
 import CardHeaderTablePedido from "@/Components/CardHeaderTablePedido";
-import { CardReadMore } from "@/Components/CardReadMore";
+import EnunciadoPedidos from "../../../Components/EnunciadoPedidos";
 
 function EsicIndex() {
     const { props } = usePage();
@@ -20,13 +15,12 @@ function EsicIndex() {
                 <BaseLayout>
                     <div className="w-full h-full flex flex-col gap-4">
                         <div className="w-full flex flex-row justify-between items-center gap-4 bg-gray-50 dark:bg-blue-800 rounded-md p-4">
-                            
-                            <CardReadMore title={"Informação ao Cidado - E-SIC"} resumo={`O Sistema Eletrônico do Serviço de
-                                    Informações ao Cidadão (e-SIC) permite que
-                                    qualquer pessoa, física ou jurídica,
-                                    encaminhe pedidos de acesso à informação...`} />
-                            <CardReadMore title={"Ouvidoria - E-OUV"} resumo={`O e-Ouv é um canal para você apresentar sugestões, elogios, solicitações, reclamações e denúncias. No serviço público, a ouvidoria é uma espécie de “ponte” entre você e a Administração Pública...`} />
-                            <CardReadMore title={"Proteção de Dados - LGPD"} resumo={`A Lei Geral de Proteção de Dados (LGPD) é uma legislação brasileira que tem como objetivo principal proteger os dados pessoais dos cidadãos...`} />
+                            <EnunciadoPedidos
+                                title={"Informação ao Cidado - E-SIC"}
+                                resumo={
+                                    "O Sistema Eletrônico do Serviço de Informações ao Cidadão (e-SIC) permite que qualquer pessoa, física ou jurídica, encaminhe pedidos de acesso à informação, acompanhe o prazo e receba a resposta da solicitação realizada para órgãos e entidades da administração. O cidadão ainda pode entrar com recursos e apresentar reclamações sem burocracia."
+                                }
+                            />
                         </div>
                         <Card className="h-full w-full bg-gray-50 dark:bg-blue-800 ">
                             <CardHeader
