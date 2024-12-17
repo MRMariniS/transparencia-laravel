6d97e84d4e00e9d37e4d45373cb5747a55902727
Marcio Roberto Marini de Sousa <marcio@publica-ro.com.br>
2024-12-17 16:34:25 -0400

    Ajustes vite

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/js/app.jsx',
-            ],
+            input: ["resources/js/app.jsx", "resources/css/app.css"],
             refresh: true,
         }),
         react(),
     ],
-    server:{
-        port:5174,
-        host:true,
-        hmr:{
-            port:5174,
-            host: 'localhost'
-        }}
+    server: {
+        port: 5174,
+        host: true,
+        hmr: {
+            port: 5174,
+            host: "localhost",
+        },
+    },
 });
